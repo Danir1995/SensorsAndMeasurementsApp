b29e97f79e6bd764a6f52eb90a7632cc92d49ce9
DanirPT <danir.hucaev@gmail.com>
2023-08-18 00:25:56 +0100

    added converters and business logic in WeatherService class

--- src/main/java/com/danir/SensorsApp/services/WeatherService.java
+++ src/main/java/com/danir/SensorsApp/services/WeatherService.java
@@ -1,9 +1,11 @@
 package com.danir.SensorsApp.services;
 
+import com.danir.SensorsApp.dto.WeatherDTO;
 import com.danir.SensorsApp.models.Weather;
 import com.danir.SensorsApp.repositories.WeatherRepository;
 import com.danir.SensorsApp.util.NoSensorException;
 import com.danir.SensorsApp.util.SensorErrorResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,21 +16,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class WeatherService {
     private final WeatherRepository weatherRepository;
     private final SensorService sensorService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public WeatherService(WeatherRepository weatherRepository, SensorService sensorService) {
+    public WeatherService(WeatherRepository weatherRepository, SensorService sensorService, ModelMapper modelMapper) {
         this.weatherRepository = weatherRepository;
         this.sensorService = sensorService;
+        this.modelMapper = modelMapper;
     }
 
-    public List<Weather> findAll() {
-        return weatherRepository.findAll();
+    public List<WeatherDTO> getAllMeasurements() {
+        return weatherRepository.findAll().stream().map(this::convertToWeatherDTO)
+                .collect(Collectors.toList());
     }
 
     public Weather findOne(int id) {
@@ -41,9 +47,9 @@ public class WeatherService {
     }
 
     @Transactional
-    public void addWeather(Weather measurement){
-        enrichMeasurement(measurement);
-        weatherRepository.save(measurement);
+    public void addWeather(WeatherDTO measurement){
+        enrichMeasurement(convertToWeather(measurement));
+        weatherRepository.save(convertToWeather(measurement));
     }
 
     public void enrichMeasurement(Weather measurement) throws NoSensorException{
@@ -60,5 +66,14 @@ public class WeatherService {
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+
+    private WeatherDTO convertToWeatherDTO(Weather weather){
+        return modelMapper.map(weather, WeatherDTO.class);
+    }
+
+    private Weather convertToWeather(WeatherDTO weatherDTO){
+        return modelMapper.map(weatherDTO, Weather.class);
+    }
+
 
 }
